cd23c95330976a46933ac0f02070d738d1b8aced
Federico Ferri <federico.ferri.it@gmail.com>
2015-11-20 16:12:28 +0100

    fix wolf goat cabbage

--- domains/wolf_goat_cabbage.pl
+++ domains/wolf_goat_cabbage.pl
@@ -1,78 +1,46 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% CAPRA E CAVOLO IN B (Boolean Fluents --- SICStus 3 e SICStus4)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% :-sicsplan(23).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% "wolf goat cabbage" puzzle
+% optimal solution takes 20 actions
 
-ogg(capra). ogg(cavolo). ogg(lupo). ogg(uomo).
+object(wolf).
+object(goat).
+object(cabbage).
+object(man).
 
-lato(sin). lato(des).
+side(left).
+side(right).
 
-pos(X) :- lato(X).
-pos(barca).
+location(X) :- side(X).
+location(boat).
 
-%%%%%%%%%%%%%%%%%%%%%
+fluent(at(X,Y)) :- object(X), location(Y).
+fluent(boat_at(Y)) :- side(Y).
+fluent(alive).
 
-fluent(sta_in(X,Y)) :- ogg(X), pos(Y).
-fluent(barca_in(Y)) :- lato(Y).
-fluent(vivi).
+action(navigate(A,B)) :-  side(A), side(B), diff(A,B).
+action(pick(A)) :- object(A).
+action(drop(A)) :- object(A).
 
-%%%%%%%%%%%%%%%%
+executable(navigate(A,B), [boat_at(A),at(man,boat)]) :- action(navigate(A,B)). 
+executable(pick(A), [boat_at(L),at(A,L)]) :- action(pick(A)).
+executable(drop(A), [at(A,boat)]) :- action(drop(A)).
 
-action(naviga(A,B)) :-  lato(A),lato(B), neq(A,B).
-action(sali(A)) :- ogg(A).
-action(scendi(A)) :- ogg(A).
+causes(navigate(A,B), boat_at(B), []) :- action(navigate(A,B)), fluent(boat_at(B)).
+causes(pick(A), at(A,boat), []) :- action(pick(A)), fluent(at(A,boat)).
+causes(drop(A), at(A,L), [boat_at(L)]) :- action(drop(A)), fluent(at(A,L)), fluent(boat_at(L)).
 
-%%%%%%%%%%%%%%%
+initially(at(A,left)) :- object(A).
+initially(boat_at(left)).
+initially(alive).
 
-executable(naviga(A,B),[barca_in(A),sta_in(uomo,barca)]) :-
-       lato(A),lato(B), neq(A,B). %%% action(naviga(A,B)).
-executable(sali(A),[barca_in(L),sta_in(A,L)]) :-
-       ogg(A),lato(L).
-executable(scendi(A),[sta_in(A,barca)]) :-
-       ogg(A).
+goal(at(A,right)) :- object(A).
+goal(alive).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+caused([at(O,L1)], mneg(at(O,L2))) :- fluent(at(O,L1)), fluent(at(O,L2)), diff(L1,L2).
+caused([boat_at(L1)], mneg(boat_at(L2))) :- fluent(boat_at(L1)), fluent(boat_at(L2)), diff(L1,L2).
 
-causes(naviga(A,B), barca_in(B), []):-
-    lato(A),lato(B),neq(A,B).
-causes(sali(A), sta_in(A,barca),[]) :-
-    ogg(A).
-causes(scendi(A), sta_in(A,L),[barca_in(L)]) :-
-    ogg(A),lato(L).
-        
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         
+% no more than 1 objects other than man on boat
+caused([at(A,boat),at(B,boat)], mneg(alive)) :- fluent(at(A,boat)), fluent(at(B,boat)), diff(A,B,man).
 
-initially(sta_in(A,sin)) :- ogg(A).
-initially(barca_in(sin)).
-initially( vivi ).
-
-%%%%%%%%%%%%%%%
-
-goal(sta_in(A,des)) :- ogg(A).
-goal( vivi ).
-
-%%%% UNIVOCITA' POSIZIONE  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         
-
-caused([sta_in(Ogg,L1)],neg(sta_in(Ogg,L2))) :-
-      ogg(Ogg),pos(L1),pos(L2),neq(L1,L2).  
-caused([barca_in(L1)],neg(barca_in(L2))) :-
-      lato(L1),lato(L2),neq(L1,L2). 
-
-%%%% Al piu' uno in barca (oltre al pastore)  %%%%%%%%%%%%%%%%%%%      
-
-caused([sta_in(A,barca),sta_in(B,barca)],neg(vivi)) :-
-      ogg(A),ogg(B),diff(A,B,uomo).
-
-%%% Mai soli capra e cavolo O lupo e capra
-
-caused([sta_in(lupo,L),sta_in(capra,L),neg(sta_in(uomo,L))],neg(vivi)) :-
-      pos(L).
-caused([sta_in(cavolo,L),sta_in(capra,L),neg(sta_in(uomo,L))],neg(vivi)) :-
-      pos(L).
-
-      
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%         
-
+caused([at(wolf,L),at(goat,L),mneg(at(man,L))], mneg(alive)) :- location(L).
+caused([at(cabbage,L),at(goat,L),mneg(at(man,L))],mneg(alive)) :- location(L).
 
